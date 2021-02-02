ab1802f6390442f542dd43a4293f780441177c84
TamerMansour0110 <78092103+TamerMansour0110@users.noreply.github.com>
2021-02-02 09:24:43 +0200

    This is Task 2 Oop by java

---
+++ src/School/Admin.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package School;
+
+import java.util.Date;
+
+/**
+ *
+ * @author tm2
+ */
+public class Admin extends Person {
+
+    public Admin(int id, String name, Date dateOfBirth, char gender) {
+        super(id, name, dateOfBirth, gender);
+    }
+
+}
